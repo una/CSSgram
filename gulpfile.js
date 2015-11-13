9b375e6cfe2810c653fc841152c7ed1abaec7494
Hugo Giraudel <hugo.giraudel@gmail.com>
2015-11-13 16:17:19 +0100

    Slightly improved the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,110 +2,79 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     rename      = require('gulp-rename'),
     cssmin      = require('gulp-minify-css'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    cache       = require('gulp-cached'),
     prefix      = require('gulp-autoprefixer'),
     browserSync = require('browser-sync'),
     reload      = browserSync.reload,
     size        = require('gulp-size'),
-    imagemin    = require('gulp-imagemin'),
-    minifyHTML  = require('gulp-minify-html'),
-    pngquant    = require('imagemin-pngquant'),
     plumber     = require('gulp-plumber'),
     deploy      = require('gulp-gh-pages'),
     notify      = require('gulp-notify'),
     sassLint    = require('gulp-sass-lint'),
     twig        = require('gulp-twig');
 
+var onError = function (err) {
+  notify.onError({
+    title:    'Gulp',
+    subtitle: 'Failure!',
+    message:  'Error: <%= error.message %>',
+    sound:    'Beep'
+  })(err);
 
-gulp.task('lib-scss', function() {
-    var onError = function(err) {
-      notify.onError({
-          title:    "Gulp",
-          subtitle: "Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-      })(err);
-      this.emit('end');
-  };
+  this.emit('end');
+};
 
-  return gulp.src('source/scss/**/*.scss')
-    .pipe(plumber({errorHandler: onError}))
+function compileSCSS (prefix) {
+  return gulp.src(prefix + '/scss/**/*.scss')
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(sass())
     .pipe(size({ gzip: true, showFiles: true }))
     .pipe(prefix())
-    .pipe(gulp.dest('source/css'))
-    .pipe(reload({stream:true}))
+    .pipe(gulp.dest(prefix + '/css'))
+    .pipe(reload({ stream: true }))
     .pipe(cssmin())
     .pipe(size({ gzip: true, showFiles: true }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('source/css'))
+    .pipe(gulp.dest(prefix + '/css'));
+}
+
+gulp.task('scss:lib', function () {
+  return compileSCSS('source')
     .pipe(gulp.dest('site/css'));
 });
 
-gulp.task('site-scss', function() {
-    var onError = function(err) {
-      notify.onError({
-          title:    "Gulp",
-          subtitle: "Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-      })(err);
-      this.emit('end');
-  };
-
-  return gulp.src('site/scss/**/*.scss')
-    .pipe(plumber({errorHandler: onError}))
-    .pipe(sass())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(prefix())
-    .pipe(gulp.dest('site/css'))
-    .pipe(reload({stream:true}))
-    .pipe(cssmin())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('site/css'));
+gulp.task('scss:site', function () {
+  return compileSCSS('site');
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "site"
-        }
-    });
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: 'site'
+    }
+  });
 });
 
 gulp.task('deploy', function () {
-    return gulp.src('site/**/*')
-        .pipe(deploy());
+  return gulp.src('site/**/*')
+    .pipe(deploy());
 });
 
-gulp.task('sass-lint', function () {
-  gulp.src('scss/**/*.scss')
+gulp.task('scss:lint', function () {
+  return gulp.src('scss/**/*.scss')
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError());
 });
 
 gulp.task('twig', function () {
-  gulp.src('site/**/*.twig', {base: './'})
+  return gulp.src('site/**/*.twig', { base: './' })
     .pipe(twig())
     .pipe(gulp.dest('./'));
 });
 
-
-gulp.task('watch', function() {
-  gulp.watch('source/scss/**/*.scss', ['lib-scss', 'site-scss', 'sass-lint']);
-  gulp.watch('source/scss/**/*.html', ['minify-html']);
+gulp.task('watch', function () {
+  gulp.watch('source/scss/**/*.scss', ['scss:lib', 'scss:lint']);
   gulp.watch('site/**/*.twig', ['twig']);
 });
 
-
-gulp.task('jshint', function() {
-  gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-gulp.task('default', ['browser-sync', 'twig', 'lib-scss', 'site-scss', 'watch']);
+gulp.task('default', ['browser-sync', 'twig', 'scss:lib', 'scss:site', 'watch']);
