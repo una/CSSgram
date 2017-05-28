44495618ce6d47a54070299c9b7e041dd20e4ec0
Teffen Ellis (Eric) <teffen@nirri.us>
2017-05-27 21:50:33 -0700

    Update app to match code style.

--- install/helper.js
+++ install/helper.js
@@ -1,65 +1,58 @@
-(function(){
+(function () {
+  if (!document.documentElement.classList) return
 
-  if (!document.documentElement.classList)
-    return;
+  var options = INSTALL_OPTIONS
+  var prevFilters = []
 
-  var options = INSTALL_OPTIONS;
-  var prevFilters = [];
-
-  var update = function(){
-    for (var i=0; i < prevFilters.length; i++){
-      prevFilters[i][0].classList.remove(prevFilters[i][1]);
+  function updateElements () {
+    for (var i = 0; i < prevFilters.length; i++) {
+      prevFilters[i][0].classList.remove(prevFilters[i][1])
     }
-    prevFilters = [];
+    prevFilters = []
 
-    for (var i=0; i < options.regions.length; i++){
-      var region = options.regions[i];
+    for (var i = 0; i < options.regions.length; i++) {
+      var region = options.regions[i]
 
-      var el = document.querySelector(region.location);
-      if (!el)
-        continue;
+      var el = document.querySelector(region.location)
+      if (!el) { continue }
 
-      var imgs = el.querySelectorAll('img');
-      if (!imgs)
-        continue;
+      var imgs = el.querySelectorAll('img')
+      if (!imgs) { continue }
 
-      for (var j=0; j < imgs.length; j++){
-        if (imgs[j].parentNode.tagName == 'FIGURE'){
-          var figure = imgs[j].parentNode;
+      for (var j = 0; j < imgs.length; j++) {
+        if (imgs[j].parentNode.tagName == 'FIGURE') {
+          var figure = imgs[j].parentNode
         } else {
-          var figure = document.createElement('figure');
+          var figure = document.createElement('figure')
 
-          var style = getComputedStyle(imgs[j]);
-          if (style.display == 'inline')
-            figure.style.display = 'inline-block';
-          else
-            figure.style.display = style.display;
+          var style = getComputedStyle(imgs[j])
+          figure.style.display = style.display == 'inline' ? 'inline-block' : style.display
 
-          figure.style.margin = style.margin;
-          imgs[j].style.margin = 0;
+          figure.style.margin = style.margin
+          imgs[j].style.margin = 0
 
-          imgs[j].parentNode.replaceChild(figure, imgs[j]);
-          figure.appendChild(imgs[j]);
+          imgs[j].parentNode.replaceChild(figure, imgs[j])
+          figure.appendChild(imgs[j])
         }
 
-        figure.classList.add(region.filter);
+        figure.classList.add(region.filter)
 
-        prevFilters.push([figure, region.filter]);
+        prevFilters.push([figure, region.filter])
       }
     }
-  };
+  }
 
-  if (document.readyState == 'loading')
-    document.addEventListener('DOMContentLoaded', update);
-  else
-    update();
+  if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', updateElements)
+  } else {
+    updateElements()
+  }
 
-  window.CSSGramInstall = {
-    setOptions: function(opts){
-      options = opts;
+  window.INSTALL_SCOPE = {
+    setOptions: function (nextOptions) {
+      options = nextOptions
 
-      update();
+      updateElements()
     }
-  };
-
-})();
+  }
+})()
